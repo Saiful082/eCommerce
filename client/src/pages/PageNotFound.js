5c45e7d126e006aed01b307b3a2f91be57a5e0e6
Saiful082 <smsaifulislam082@gmail.com>
2023-08-10 11:54:50 +0600

    navbar added and page create

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Layout from '../components/Layout/Layout';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
     return (
-        <div>
-            <h1>Page Not found</h1>
-        </div>
+        <Layout>
+            <div className='pnf'>
+                <h1 className='pnf-title'>404</h1>
+                <h2 className='pnf-heading'>Oops ! Page Not Found</h2>
+                <Link to='/' className='pnt-btn'> 
+                Go Back
+                </Link>
+           </div>
+        </Layout>
     );
 };
 
