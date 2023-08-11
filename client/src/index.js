6c52f561fd9a373220c9df09aedb9113f2835a10
Saiful082 <smsaifulislam082@gmail.com>
2023-08-12 01:40:46 +0600

    context api create

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import { AuthProvider } from './context/ContextAuth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+ <AuthProvider>
+   <BrowserRouter>
     <App />
   </BrowserRouter>
+ </AuthProvider>
  
 );
 
