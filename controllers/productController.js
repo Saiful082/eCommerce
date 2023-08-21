bdc04559c8c72ebbeb3d2f76cdd77589e48955a3
Saiful082 <smsaifulislam082@gmail.com>
2023-08-21 16:13:17 +0600

    product api running

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,37 @@
+import slugify from "slugify";
+import productModel from "../models/productModel.js";
+import fs from 'fs';
+
 export const createProductController = async(req, res) => {
     try {
-        
+        const {name, slug, description, price, category, quantity, shipping} = req.fields;
+        const {photo} = req.files
+        // validation
+        switch(true){
+            case !name: 
+            return res.status(500).send({error:'Name Is required'})
+            case !description: 
+            return res.status(500).send({error:'description Is required'})
+            case !price: 
+            return res.status(500).send({error:'price Is required'})
+            case !category: 
+            return res.status(500).send({error:'category Is required'})
+            case !quantity: 
+            return res.status(500).send({error:'quantity Is required'})
+            case photo && photo.size > 2000000: 
+            return res.status(500).send({error:'Photo Is required and should be less then 2mb'})
+        }
+        const products = new productModel({...req.fields, slug:slugify(name)})
+        if(photo){
+            products.photo.data = fs.readFileSync(photo.path)
+            products.photo.contentType = photo.type
+        }
+        await products.save()
+        res.status(201).send({
+            success: true,
+            message: 'Product Create Successfully',
+            products,
+        })
     } catch (error) {
         console.log(error);
         res.status(500).send({
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectDb from "./config/db.js";
 import authRoutes from './routes/authRoute.js'
 import cors from 'cors';
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoute from './routes/productRouter.js';
 
 // Configure env
 dotenv.config();
@@ -25,7 +26,7 @@ app.use(morgan('dev'));
 //routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
-app.use('/api/v1/product', productRoutes);
+app.use('/api/v1/product', productRoute);
 // Rest Api
 app.get('/', (req, res) => {
 res.send('<h1> Welcome To eCommerce app</h1>')
